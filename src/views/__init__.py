bd61665df1ce233ce99c5f2e56a95540b87abc48
kentheweb <kenthewebmaster@gmail.com>
2020-06-17 14:50:40 +0100

    worked up with the styling

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,17 +1,30 @@
 # the views should be put here
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
 
 home_blueprint = Blueprint('users', __name__, template_folder='../templates/')
 
 
 @home_blueprint.route('/home')
 def home():
-    return render_template('other/index.html')
+    return render_template('other/index.html', title='home')
 
 
 @home_blueprint.route('/about')
 def about():
-    return render_template('other/about.html')
+    return render_template('other/about.html', title='about')
 
 
+@home_blueprint.route('/work/api')
+def api():
+    return jsonify({
+        'hello': 'welcome'
+    })
 
+
+@home_blueprint.route('/register/api')
+def register_api():
+    if request.method == 'GET':
+        pass
+
+    else:
+        pass
